218463364408fbfbff9bd9d75cce1e31c54249fa
Carlos Ochoa <carlosoaly@gmail.com>
2022-11-25 16:40:53 -0500

    Correccion para formato html incorrecto

--- google_api/html_parse.py
+++ google_api/html_parse.py
@@ -6,8 +6,8 @@ def get_registro_solicitud (html:str):
     soup = BeautifulSoup(html,"html.parser")
     rows=soup.find_all('tr')
     values=rows[1]
-    columns=values.find_all("td")
-    iterator = map(lambda data:unidecode(data.string), columns)
+    column_tags=values.find_all("td")
+    iterator = map(lambda  data: ('' if data.string==None else unidecode(data.string)), column_tags)
     return list(iterator)
 
 if __name__ == '__main__':
